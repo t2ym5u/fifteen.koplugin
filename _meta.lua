a8cdf72e59963c2dabdd6b36e3496ac31af208e1
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-29 14:50:35 +0200

    chore: bump version to 1.1.14

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Fifteen Puzzle"),
     description = _("Sliding tile puzzle. Reassemble the scrambled picture."),
-    version     = "1.1.13",
+    version     = "1.1.14",
 }
