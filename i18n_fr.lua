e190df298a6ea2eca0921f58be99be02fa2b636f
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:58:34 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,6 +1,6 @@
 return {
-    ["Moves: %1  Time: %2  Best: %3"]        = { fr = "Déplacements : %1  Temps : %2  Meilleur : %3" },
-    ["Solved in %1 moves! Best: %2"]          = { fr = "Résolu en %1 coups ! Meilleur : %2" },
-    ["Fifteen Puzzle"]                        = { fr = "Taquin" },
-    ["Sliding tile puzzle. Reassemble the scrambled picture."] = { fr = "Puzzle à tuiles coulissantes. Reconstituez l'image mélangée." },
+    ["Moves: %1  Time: %2  Best: %3"]        = { fr = "Déplacements : %1  Temps : %2  Meilleur : %3", es = "Movimientos: %1  Tiempo: %2  Mejor: %3", de = "Züge: %1  Zeit: %2  Beste: %3" },
+    ["Solved in %1 moves! Best: %2"]          = { fr = "Résolu en %1 coups ! Meilleur : %2", es = "¡Resuelto en %1 movimientos! Mejor: %2", de = "Gelöst in %1 Zügen! Beste: %2" },
+    ["Fifteen Puzzle"]                        = { fr = "Taquin", es = "Puzle del Quince", de = "15er-Puzzle" },
+    ["Sliding tile puzzle. Reassemble the scrambled picture."] = { fr = "Puzzle à tuiles coulissantes. Reconstituez l'image mélangée.", es = "Puzle de fichas deslizantes. Recompón la imagen desordenada.", de = "Schiebepuzzle. Setze das durcheinandergebrachte Bild wieder zusammen." },
 }
